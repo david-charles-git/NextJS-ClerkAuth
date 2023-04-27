cd7f65db78d3835c3006e1fd7cfb35026ded7d57
David <david.charles@addmustard.com>
2023-04-27 19:05:04 +0100

    general commit

---
+++ app/public/dist/page.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var publicPage = function () {
+    return (React.createElement("main", { id: "PublicPage" },
+        React.createElement("h1", null, "Public Page")));
+};
+exports["default"] = publicPage;
+
+//# sourceMappingURL=page.js.map
---
+++ app/public/page.tsx
@@ -0,0 +1,8 @@
+const publicPage : React.FC = () => {
+	return (
+		<main id="PublicPage">
+			<h1>Public Page</h1>
+		</main>
+	)
+};
+export default publicPage;
