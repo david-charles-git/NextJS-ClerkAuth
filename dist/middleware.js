43377f44a1875d398b4a4574e0d3718889ab9060
David <david.charles@addmustard.com>
2023-04-27 10:26:06 +0100

    update to middleware

--- dist/middleware.js
+++ dist/middleware.js
@@ -13,18 +13,19 @@ var isPublic = function (path) {
     var isPublic = publicPaths.find(function (x) { return path.match(new RegExp(("^" + x + "$").replace('*$', '($|/)'))); });
     return isPublic;
 };
-exports["default"] = server_1.withClerkMiddleware(function (request) {
+var middleWare = server_1.withClerkMiddleware(function (request) {
     if (isPublic(request.nextUrl.pathname)) {
         return server_2.NextResponse.next();
     }
     var userId = server_1.getAuth(request).userId;
     if (!userId) {
         var signInUrl = new URL('/signin/', request.url);
-        //signInUrl.searchParams.set('redirect_url', request.url)
+        //signInUrl.searchParams.set('redirect_url', request.url);
         return server_2.NextResponse.redirect(signInUrl);
     }
     return server_2.NextResponse.next();
 });
+exports["default"] = middleWare;
 exports.config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
 
 //# sourceMappingURL=middleware.js.map
--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,30 @@
-import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // Set the paths that don't require the user to be signed in
 const publicPaths : Array<string> = ['/', '/signin*', '/signup*', '/public*'];
 
-const isPublic = (path: string) => {
-  const isPublic = publicPaths.find(x => path.match(new RegExp(`^${x}$`.replace('*$', '($|/)'))));
+const isPublic : (path : string) => string | undefined = (path) => {
+	const isPublic : string | undefined = publicPaths.find(x => path.match(new RegExp(`^${x}$`.replace('*$', '($|/)'))));
 
-  return isPublic;
-}
+	return isPublic;
+};
 
-export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) { return NextResponse.next(); }
-  
-  const { userId } = getAuth(request);
+const middleWare = withClerkMiddleware((request: NextRequest) => {
+    if (isPublic(request.nextUrl.pathname)) { return NextResponse.next(); }
 
-  if (!userId) {    
-    const signInUrl = new URL('/signin/', request.url)
-    //signInUrl.searchParams.set('redirect_url', request.url)
+	const { userId } = getAuth(request);
 
-    return NextResponse.redirect(signInUrl)
-  }
+	if (!userId) {    
+		const signInUrl : URL = new URL('/signin/', request.url);
+		//signInUrl.searchParams.set('redirect_url', request.url);
 
-  return NextResponse.next();
-})
+		return NextResponse.redirect(signInUrl);
+	}
 
+	return NextResponse.next();
+});
+
+export default middleWare;
 export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)'};
